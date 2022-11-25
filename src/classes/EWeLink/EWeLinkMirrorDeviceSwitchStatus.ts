5e26a2f05b7242e82d82a0893a9140505685067a
“nigelsdtech” <nigelsdtech@gmail.com>
2022-11-25 20:22:24 +0000

    Run the follow check when a device first comes online

--- src/classes/EWeLink/EWeLinkMirrorDeviceSwitchStatus.ts
+++ src/classes/EWeLink/EWeLinkMirrorDeviceSwitchStatus.ts
@@ -16,6 +16,7 @@ export default class EWeLinkMirrorDeviceSwitchStatus extends Observer {
         private readonly eWeLinkConnection: eWeLinkApi
     ) {
         super(log, name, errorNotifier)
+ 
         this.log.info(`[${this.name}] Listening for ${sourceDeviceId} and updating ${satelliteDeviceId}`)
     }
 
@@ -25,33 +26,71 @@ export default class EWeLinkMirrorDeviceSwitchStatus extends Observer {
         if (typeof message != 'object'
             || message.deviceid != this.sourceDeviceId
             || !message.params
-            || !message.params.switch
-            || ["on", "off"].indexOf(message.params.switch) == -1
+            || !message.action
         ) {
-            this.log.debug(`[${this.name}]: Discarding message on device ${message.deviceid}`)
+            this.log.debug(`[${this.name}] Discarding irrelevant message`)
             return
         }
-        
-        this.updateSwitch(message.params.switch)
+
+        if (message.action == "sysmsg") return this.processSysMsg(message);
+
+        if (message.action == "update" 
+            && message.params
+            && message.params.switch
+            && ["on", "off"].indexOf(message.params.switch) > -1)
+        {
+            return this.updateSwitch(message.params.switch)
+        }
+
+        this.log.debug(`[${this.name}] Discarding message`)
 
     }
 
-    private async updateSwitch(status: 'on' | 'off'): Promise<void> {
-        this.log.info(`[${this.name}]: Setting new switch status on ${this.satelliteDeviceId} to ${status}...`)
+    private async processSysMsg(message: {
+        params: {
+            online: boolean
+        }
+    }): Promise<void> {
+
+        this.log.debug(`[${this.name}] Processing sysmsg ${JSON.stringify(message,null,'\t')}`)
+
+        if (!message.params || !message.params.online) {
+            this.log.info(`[${this.name}] Discarding as irrelevant`)
+            return
+        }
+
+        this.log.info(`[${this.name}]: Device has come online. Getting power status...`)
+        const resp = await this.eWeLinkConnection.getDevicePowerState(this.sourceDeviceId)
+
+        if (resp.error) return this.sendError(`Error getting device state - error: ${resp.error}, msg: ${resp.msg}`);
+
+        if (!resp.state || ["on", "off"].indexOf(resp.state) == -1) return;
+
+        this.log.info(`[${this.name}] Power state is ${resp.state}`)
+        return this.updateSwitch(resp.state as "on" | "off")
+    }
+
+    private async updateSwitch(state: 'on' | 'off'): Promise<void> {
+        this.log.info(`[${this.name}] Setting new switch state on ${this.satelliteDeviceId} to ${state}...`)
 
         try {
-            const resp = await this.eWeLinkConnection.setDevicePowerState(this.satelliteDeviceId,status)
-            if ((resp).msg) this.sendError(`code: ${resp.error}, message: ${resp.msg}`)
-            this.log.info(`[${this.name}]: Set new switch status on ${this.satelliteDeviceId} to ${resp.state}.`)
+
+            const resp = await this.eWeLinkConnection.setDevicePowerState(this.satelliteDeviceId,state)
+
+            if ((resp).msg) this.sendError(`Error setting power state to ${state} - code: ${resp.error}, message: ${resp.msg}`)
+
+            this.log.info(`[${this.name}] Set new switch status on ${this.satelliteDeviceId} to ${resp.state}.`)
+
         } catch (e) {
             const err = e as Error
-            this.sendError(`[${this.name}]: Update error- ${err.message}`)
+            this.sendError(`Update error - ${err.message}`)
         }
     }
 
-    private async sendError(errMsg:string) {
-        this.log.error(`[${this.name}]: ${errMsg}`)
-        this.errorNotifier({errMsg})
+    private sendError(rawMsg:string) {
+        const errMsg = `[${this.name}]: ${rawMsg}`
+        this.log.error(errMsg)
+        this.errorNotifier({errMsg: errMsg})
     }
 
 }
--- test/unit/classes/EWeLink/EWeLinkMirrorDeviceSwitchStatus.ts
+++ test/unit/classes/EWeLink/EWeLinkMirrorDeviceSwitchStatus.ts
@@ -1,6 +1,6 @@
 import { should } from 'chai';
 import 'mocha';
-import {stub} from 'sinon' 
+import {SinonStub, stub} from 'sinon' 
 import { eWeLinkApi, iLogger } from '../../../../src/interfaces/commonInterfaces';
 import loggerStub from '../../../stubs/loggerStub';
 const e = require('ewelink-api')
@@ -10,7 +10,11 @@ should()
 
 describe('EWeLinkMirrorDeviceSwitchStatus', ()=>{
 
-    const eWeLinkConnectionStub: eWeLinkApi = e;
+    const eWeLinkConnectionStub: eWeLinkApi = new e({
+        email: "fake@fake.com",
+        password: "fakepassword",
+        region: "fakeplace"
+    });
 
     const log: iLogger = loggerStub();
 
@@ -18,57 +22,138 @@ describe('EWeLinkMirrorDeviceSwitchStatus', ()=>{
         log,
         'test',
         ()=>{},
-        '12345',
+        'myDeviceId',
         '6789',
         eWeLinkConnectionStub
     )
 
     describe('update()', () => {
 
-        const s = stub()
-        elmdss['updateSwitch'] = s;
+        const setPowerStateStub = stub()
+        elmdss['updateSwitch'] = setPowerStateStub;
+
+        const getPowerStateStub = stub()
+        elmdss['eWeLinkConnection']['getDevicePowerState'] = getPowerStateStub
+
+        const sendErrorStub = stub()
+        elmdss['sendError'] = sendErrorStub
+
+        const allStubs = [
+            setPowerStateStub,
+            getPowerStateStub,
+            sendErrorStub
+        ]
 
         beforeEach(() => {
-            if (s.called) {s.reset()}
+            resetStubs(allStubs)
+            getPowerStateStub.throws('Should not have been called')
+            sendErrorStub.throws('Should not have been called')
         })
 
         after(()=> {
-            if (s.called) {s.reset()}
-            s.restore()
+            resetStubs(allStubs)
+            allStubs.forEach(s=> s.restore())
         })
 
 
         const tests = [
             "pong",
             {
-                "error": 0,
-                "apikey": "abcd-efg-hijk",
-                "config": {
+                error: 0,
+                apikey: "abcd-efg-hijk",
+                config: {
                         "hb": 1,
                         "hbInterval": 145
                 },
-                "sequence": "123456789"
+                sequence: "123456789"
             },{
                 deviceid: "notMyDevice",
                 params: {
                     switch: "on"
                 }
-        }]
+            }, {
+                action: "sysmsg",
+                apikey: "abcd-efg-hijk",
+                deviceid: "notMyDevice",
+                params: {
+                    online: true
+                }
+            }, {
+                action: "uninterestingAction",
+                deviceid: "myDeviceId",
+                params: {
+                    switch: "on"
+                }
+            }
+        ]
         tests.forEach((t,i) => {
             it(`Discards an irrelevant message [${i}]`, async ()=> {
                 elmdss.update(t)
-                s.called.should.be.false
+                setPowerStateStub.called.should.be.false
             })
         })
 
-        it('Accepts a relevant message', async ()=> {
-            elmdss.update({
-                deviceid: "12345",
+        it('Accepts a relevant update message', async ()=> {
+            await elmdss.update({
+                action: "update",
+                deviceid: "myDeviceId",
                 params: {
                     switch: "on"
                 }
             })
-            s.called.should.be.true
+            setPowerStateStub.called.should.be.true
+        })
+
+        describe('Receiving a system message', async ()=> {
+
+            beforeEach(()=> {
+                resetStubs([getPowerStateStub, sendErrorStub])
+            })
+
+            it("Bugs out if it can't get device power status", async () => {
+                getPowerStateStub.withArgs("myDeviceId").resolves({
+                    error: "Fake error",
+                    msg: "Fake message"
+                })
+
+                await elmdss.update({
+                    action: "sysmsg",
+                    apikey: "abcd-efg-hijk",
+                    deviceid: "myDeviceId",
+                    params: {
+                        online: true
+                    }
+                })
+
+                getPowerStateStub.calledWith("myDeviceId").should.be.true;
+                sendErrorStub.calledWith(`Error getting device state - error: Fake error, msg: Fake message`).should.be.true
+                setPowerStateStub.called.should.be.false
+            })
+
+            it ("Attempts to change power status on the relevant device", async () => {
+                getPowerStateStub.withArgs("myDeviceId").resolves({
+                    status: 'ok',
+                    state: 'off',
+                    channel: 1
+                })
+
+                await elmdss.update({
+                    action: "sysmsg",
+                    apikey: "abcd-efg-hijk",
+                    deviceid: "myDeviceId",
+                    params: {
+                        online: true
+                    }
+                })
+
+                getPowerStateStub.calledWith("myDeviceId").should.be.true;
+                sendErrorStub.called.should.be.false
+                setPowerStateStub.calledWith("off").should.be.true
+            })
         })
     })
 })
+
+function resetStubs(stubs: SinonStub[]): void {
+    stubs.forEach(s=>s.reset())
+}
